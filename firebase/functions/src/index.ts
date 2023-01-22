d6db481fa385cf6a666af9a8ecd1e002d0643d8e
Tim <16493467+Dev10000@users.noreply.github.com>
2023-01-22 06:54:57 +0200

    Server Implement Delete Stale History Background Process
    - Delete stale history older than 10 minutes background process

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -8,7 +8,14 @@ admin.initializeApp();
 export * from './endpoints';
 
 /**
+ * Scheduler - Delete stale history every 10 minutes
+ */
+export * from './schedulers';
+
+/**
  * Triggers - Background data processing
  * - Fetch Data and Update Database - Long Polling Data
+ * - Update drone history every time snapshot is updated (every 2 seconds)
+ * - Calculate shortest distance every time 'history/{droneId}' is updated
  */
 export * from './triggers';
---
+++ firebase/functions/src/schedulers.ts
@@ -0,0 +1,36 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+/**
+ * @description - Delete Stale History
+ * - Deletes history older than 10 minutes
+ * @function
+ * @async
+ * @return {Promise<FirebaseFirestore.WriteResult>}
+ * @throws {Error}
+ */
+
+const deleteStaleHistory = functions.pubsub
+  .schedule('every 10 minutes')
+  .onRun(async () => {
+    const oneMinute = 60000;
+    const cutoff = new Date(Date.now() - 10 * oneMinute);
+    const stale = admin
+      .firestore()
+      .collection('history')
+      .where('updatedAt', '<', cutoff);
+    try {
+      const snapshot = await stale.get();
+      const batch = admin.firestore().batch();
+      snapshot.forEach((doc) => {
+        batch.delete(doc.ref);
+      });
+      return await batch.commit();
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  });
+
+// eslint-disable-next-line import/prefer-default-export
+export { deleteStaleHistory };
